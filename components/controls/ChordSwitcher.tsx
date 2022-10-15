96ffca6b1fbe77d84c915f7ef787cbb9b2fc0452
CCColda <48625432+CCColda@users.noreply.github.com>
2022-10-15 22:06:24 +0200

    Update ChordSwitcher

--- components/controls/ChordSwitcher.tsx
+++ components/controls/ChordSwitcher.tsx
@@ -7,19 +7,21 @@ const ChordSwitcher: FC<{}> = (_props) => {
 	const dataContext = useContext(DataContext);
 
 	return <div
-		className={styles.musicControl}
-		onChange={e => {
-			console.log((e as any).target.value);
-			dataContext.fn.setMode((e.target as HTMLInputElement).value as "chord" | "note");
-		}}>
-		<label className={styles.notes}>
-			<input type="radio" name="musiccontrol" value="note" defaultChecked></input>
-			<span></span>
-		</label>
-		<label className={styles.chords}>
-			<input type="radio" name="musiccontrol" value="chord"></input>
-			<span></span>
-		</label>
+		className={styles.chordSwitcher}>
+		<button
+			className={
+				styles.note + (dataContext.data.ready && dataContext.data.mode == "note" ? " " + styles.active : "")
+			}
+			onClick={_ => dataContext.fn.setMode("note")}>
+			+
+		</button>
+		<button
+			className={
+				styles.chord + (dataContext.data.ready && dataContext.data.mode == "chord" ? " " + styles.active : "")
+			}
+			onClick={_ => dataContext.fn.setMode("chord")}>
+			𝅘𝅥+
+		</button>
 	</div>;
 }
 
