56d5ebf2a974b622b119d1b72bbad4632fc5dfab
CCColda <48625432+CCColda@users.noreply.github.com>
2022-11-15 22:19:47 +0100

    Updates

--- _next/static/q3ZUCC75X5JGk2um-GnOM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/122-412571c0566ea415.js","static/css/51f120e6ac57690d.css","static/chunks/pages/index-16aa2034cc8be1de.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
